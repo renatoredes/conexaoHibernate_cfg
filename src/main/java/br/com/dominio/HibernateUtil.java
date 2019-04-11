ab4dcc3dc16a8ffe3a4589ede4c8f274e9522243
renatoredes2016 <renatoredes2016@gmail.com>
2019-04-11 17:23:38 -0300

    apagando comentarios testes

--- src/main/java/br/com/dominio/HibernateUtil.java
+++ src/main/java/br/com/dominio/HibernateUtil.java
@@ -28,10 +28,5 @@ public class HibernateUtil {
 			System.err.println("A fábrica de sessões não pode ser criada." + ex);
 			throw new ExceptionInInitializerError(ex);
 		}
-	}/*+1+1*//*+1+1*//*+1+1*//*+1+1*//*+1+1*//*+1+1*/
+	}
 }
-/*1+1*/
-	/* TESTE */
-/*teste2*/
-
-/*teste3*/
